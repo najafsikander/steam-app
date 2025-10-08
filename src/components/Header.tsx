691c3d5d14f0fe3b960d83aacf52a2681a224c44
najafsikander <najafsikander@yahoo.com>
2025-10-08 12:04:04 +0400

    Changed Header Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,14 @@
 import { Link } from '@tanstack/react-router'
 
-import { useState } from 'react'
-import {
-  ChevronDown,
-  ChevronRight,
-  ClipboardType,
-  Home,
-  Menu,
-  Network,
-  SquareFunction,
-  StickyNote,
-  X,
-} from 'lucide-react'
-
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false)
-  const [groupedExpanded, setGroupedExpanded] = useState<
-    Record<string, boolean>
-  >({})
 
   return (
     <>
       <header className="p-4 flex items-center bg-gray-800 text-white shadow-lg">
         <Link to="/">
           <img
-            src="/tanstack-word-logo-white.svg"
-            alt="TanStack Logo"
+            src="/images/svgs/logo_steam.svg"
+            alt="Steam Logo"
             className="h-10"
           />
         </Link>
